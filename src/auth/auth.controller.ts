f8d29e53e3d7d950bdafc7a590c1f2ff51fa2e18
kg-phantom <ksurbayan@gmail.com>
2022-03-04 21:22:56 -0800

    start signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -7,7 +7,10 @@ export class AuthController{
 
     // POST /auth/signup
     @Post('signup')
-    signup() {
+    signup(@Body() dto: any) {
+        console.log({
+            dto
+        });
         return this.authService.signup();
     }
 
