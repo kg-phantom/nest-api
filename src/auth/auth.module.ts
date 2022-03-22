c123844ad7b5398ee45c9dbbaeacbd56c30c1997
kg-phantom <ksurbayan@gmail.com>
2022-03-21 23:35:49 -0700

    return jwt token on signup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
+    imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, ConfigService],
 })
 export class AuthModule {}
