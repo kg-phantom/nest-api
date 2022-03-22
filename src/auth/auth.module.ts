4a21b9d99835502dc775e2bc6c65c76e9ee11561
kg-phantom <ksurbayan@gmail.com>
2022-03-22 00:15:47 -0700

    get user info through /users/me

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./strategy";
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService, ConfigService],
+    providers: [AuthService, ConfigService, JwtStrategy],
 })
 export class AuthModule {}
